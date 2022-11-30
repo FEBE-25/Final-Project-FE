46db6f374198e7b801a0b715e8aa63ef359e0e0a
Fitron Ansori <fitronansori7@gmail.com>
2022-11-30 08:58:51 +0800

    update daftar-sesi dan history-les

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,13 +2,14 @@ import { applyMiddleware, combineReducers, createStore } from "redux";
 import thunk from "redux-thunk";
 import orderReducer from "../reducer/orderReducer";
 import userReducer from "../reducer/userReducer";
-
 import teacherReducer from "../reducer/teacherReducer";
+import scheduleReduceer from "../reducer/scheduleReducer";
 
 const allReducer = combineReducers({
   user: userReducer,
   order: orderReducer,
   teachers: teacherReducer,
+  schedules: scheduleReduceer,
 });
 const store = createStore(allReducer, applyMiddleware(thunk));
 
